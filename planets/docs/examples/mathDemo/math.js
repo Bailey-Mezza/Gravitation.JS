5e1472cf2a14dd62d431b806295017215b62844e
Bailey Mezza <bmezza2812@gmail.com>
2025-08-06 11:13:07 +0100

    Began working on math demo with a new custom terminal ui to show all bodies in a system

---
+++ planets/docs/examples/mathDemo/math.js
@@ -0,0 +1,51 @@
+import { PhysicsEngine } from '/planets/src/core/physicsEngine.js';
+
+import Sun from '/planets/src/bodies/sun.js';
+import Planet from '/planets/src/bodies/planet.js';
+
+function updateUTCClock() {
+    const clockEl = document.getElementById('utc-clock');
+    const now = new Date();
+    // format as HH:MM:SS in UTC
+    const timeStr = now.toUTCString().split(' ')[4];
+    clockEl.textContent = `UTC ${timeStr}`;
+}
+
+
+
+
+
+
+const isPausedRef = { value: false };
+const engine = new PhysicsEngine(suns, planets);
+const planets = [];
+const suns = [];
+
+suns.push(new Sun(
+    10000,                    // mass
+    { x: 0, y: 0 },           // position
+    { x: 0, y: 0 },           // velocity
+    50                        // radius
+));
+
+planets.push(new Planet(
+    1,                        // mass
+    { x: 300, y: 0 },         // position
+    { x: 0, y: 2 },           // velocity
+    10                        // radius
+));
+
+
+function loop() {
+    requestAnimationFrame(loop);
+
+    if (!isPausedRef.value) {
+        engine.simulateStep();
+    }
+    console.log("ello");
+    
+}
+
+loop();
+updateUTCClock();
+setInterval(updateUTCClock, 1000);
